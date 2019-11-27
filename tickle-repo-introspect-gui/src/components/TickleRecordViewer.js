a2d3159c0d7d21d4479db49e23991de39265fba8
Henrik Witt-Hansen <hwha@dbc.dk>
2019-11-27 16:09:08 +0100

    Initial add of stolen components from rawrepo-introspect
    In need of minor touchup work to work in ticklerepo introspect
    
    Auditors: jbn, atm

--- tickle-repo-introspect-gui/src/components/TickleRecordViewer.js
+++ tickle-repo-introspect-gui/src/components/TickleRecordViewer.js
@@ -4,6 +4,8 @@
  */
 
 import React from "react";
+import RawrepoIntrospectRecordFormatSelector from './tickle-repo-introspect-record-format-selector';
+import RawrepoIntrospectRecordCopy from './tickle-repo-introspect-record-copy';
 
 const HEIGHT_OFFSET = 165;
 
@@ -40,15 +42,37 @@ class TickleRecordViewer extends React.Component {
     render() {
         return (
 
-            <div className="flex-container">
-                <div id="content-container"
-                     style={{
-                         height: this.state.textareaHeight + 'px',
-                         overflow:'auto',
-                         border: 'solid 1px #999999'
-                     }}>
-                    <div id="content">
-                        {this.props.record}
+            <div>
+                <div style={{width: '100%', overflow: 'hidden'}}>
+                    <div className='form-group' style={{height: '28px'}}>
+                        <label
+                            className='control-label'
+                            style={{marginTop: '5px', float: 'left'}}
+                            htmlFor='record-format-selector'>Visningsformat</label>
+                        <div style={{marginLeft: '10px', float: 'left'}}>
+                            <RawrepoIntrospectRecordFormatSelector
+                                id='record-format-selector'
+                                format={this.props.format}
+                                onChangeFormat={this.props.onChangeFormat}
+                                recordLoaded={this.props.recordLoaded}/>
+                        </div>
+                        <div style={{marginLeft: '25px', float: 'left'}}>
+                            <RawrepoIntrospectRecordCopy
+                                onCopyToClipboard={this.props.onCopyToClipboard}
+                                recordLoaded={this.props.recordLoaded}/>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-container">
+                    <div id="content-container"
+                         style={{
+                             height: this.state.textareaHeight + 'px',
+                             overflow:'auto',
+                             border: 'solid 1px #999999'
+                         }}>
+                        <div id="content">
+                            {this.props.record}
+                        </div>
                     </div>
                 </div>
             </div>
